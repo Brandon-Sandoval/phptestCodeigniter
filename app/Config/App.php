6d18001582b28d12e725488d27fc7b253cd83c36
Brandon Sandoval <56595658+Brandon-Sandoval@users.noreply.github.com>
2022-06-07 22:28:47 -0400

    cambiar base_url para el deploy

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/phptestCodeigniter/';
+    public $baseURL = 'https://codeigniterwebtest.herokuapp.com';
 
     /**
      * --------------------------------------------------------------------------
